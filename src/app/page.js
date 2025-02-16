29ed6b9fac9030b66aad261edbebf6d1776d3649
krishnas05 <kanavkrishna05@gmail.com>
2025-02-16 22:12:25 +0530

    add

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,12 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Textform from "../components/Textform";
-import { useState } from "react";
 import Footer from "../components/Footer";
 
 function Home() {
-  const [mode, setMode] = useState("light");
-  const [alert, setAlert] = useState(null);
-
   return (
     <>
-      <Navbar title="TextUtils"/>
+      <Navbar/>
       <div className="container pt-0">
         <Textform />
       </div>
